2f2f070d363245287a54b9a1b6f867a5c4502381
Andreu <a.sanchez11@sapalomera.cat>
2024-04-02 11:36:40 +0200

    Pràctica 1A finalitzada

--- Practica 1A/practica1A_AndreuSanchez/src/practica1A.java
+++ Practica 1A/practica1A_AndreuSanchez/src/practica1A.java
@@ -69,6 +69,9 @@ public class practica1A {
 
     //------------------------------------------------------------------------------------------------------------------
     static void OutOfMemoryError(){
+        // OutOfMemoryError no pot ser capturat utilitzant blocs try-catch normals a Java.
+        // Això és perquè OutOfMemoryError és una subclasse d'Error, que és una classe que estén de Throwable, però no estén d'Exception.
+        // Tot i aixì et deixo la forma que jo utilitzaria per capturarla
         List<Integer> list = new ArrayList<>();
         try {
             System.out.println("Iniciant el procés per fer un OutOfMemoryError...");
@@ -77,32 +80,46 @@ public class practica1A {
             }
         } catch (OutOfMemoryError e) {
             // Captuerm l'error OutOfMemory.
-            System.out.println("S'ha produït un error durant el procés: " + e.getMessage());
+            System.out.println("---------------------------------------------------------------------------------------");
+            System.out.println("S'ha produït un error durant el procés (OutOfMemoryError): " + e.getMessage());
             System.out.println("Detalls addicionals:");
-            System.out.println("Classe de l´excepció: " + e.getClass());
-            System.out.println("Causa arrel: " + e.getCause());
+            System.out.println("       Classe de l´excepció: " + e.getClass());
+            Throwable causa = e.getCause();
+            if (causa != null) {
+                System.out.println("       La causa de l'excepció és: " + causa.getMessage());
+            } else {
+                System.out.println("       La causa de l'excepció és: Fuga de memòria.");
+            }
             System.out.println("Rastreig de la pila:");
             e.printStackTrace();
         } finally {
             // Mostrem la mida de la llista.
             System.out.println("La mida de la llista és: " + list.size());
+            //Netejem l'array per no ocupar memoria.
+            list = null;
+            System.out.println("Llista netejada");
         }
     }
     //------------------------------------------------------------------------------------------------------------------
 
     //------------------------------------------------------------------------------------------------------------------
-    // Trec el e.getMessage, perquè surt com null i el trace perquè si no surten moltes traces.
     static void stackOverFlowError() {
         try {
             System.out.println("Iniciant procés per fer un stackOverFlowError...");
             cridaRecursiva(0);
         } catch (StackOverflowError e) {
+            System.out.println("---------------------------------------------------------------------------------------");
             System.out.println("S'ha produït un error de desbordament de la pila (StackOverflowError)");
             System.out.println("Detalls addicionals:");
-            System.out.println("Classe de l'excepció: " + e.getClass());
-            System.out.println("Causa arrel: " + e.getCause());
+            System.out.println("       Classe de l´excepció: " + e.getClass());
+            Throwable causa = e.getCause();
+            if (causa != null) {
+                System.out.println("       La causa de l'excepció és: " + causa.getMessage());
+            } else {
+                System.out.println("       La causa de l'excepció és: Desbordament de la pila.");
+            }
         } finally {
-            System.out.println("Fi del mètode stackOverFlowError, l'error ha sigut capturat");
+            System.out.println("Fi del mètode stackOverFlowError.");
         }
     }
 
@@ -115,6 +132,7 @@ public class practica1A {
 
     //------------------------------------------------------------------------------------------------------------------
     static void illegalArgumentExceptionsArrays(){
+        System.out.println("Inici del mètode illegalArgumentExceptionsArrays...");
         int[] array = {10, 20, 30, 40, 50};
         Scanner scan = new Scanner(System.in);
         try {
@@ -123,15 +141,16 @@ public class practica1A {
             System.out.println("-------------------------------------------------------------------------------------");
             System.out.println("Tenim els seguents valors a la llista: "+ Arrays.toString(array));
             System.out.println("Vols canviar el valor d'alguna posició de la llista? (S/N)");
-            String response = scan.nextLine();
-            if (response.equalsIgnoreCase("S")) {
+            String resposta = scan.nextLine();
+            //EqualsIgnoreCase ignora si es mayus o minus
+            if (resposta.equalsIgnoreCase("S")) {
                 System.out.println("Introdueix l'índex de la posició a canviar (0 - " + (array.length - 1) + "):");
-                int position = scan.nextInt();
+                int posicio = scan.nextInt();
                 System.out.println("Introdueix el nou valor:");
-                int newValue = scan.nextInt();
+                int nouValor = scan.nextInt();
 
                 // Canviar el valor en la posició especificada
-                array[position] = newValue;
+                array[posicio] = nouValor;
                 System.out.println("Valor canviat correctament.");
             } else {
                 System.out.println("No s'han realitzat canvis.");
@@ -151,7 +170,7 @@ public class practica1A {
             if (causa != null) {
                 System.out.println("       La causa de l'excepció és: " + causa.getMessage());
             } else {
-                System.out.println("       L'excepció no te causa coneguda.");
+                System.out.println("       La causa de l'excepció és: No es troba l'element dins l'array.");
             }
         }
         finally {
@@ -159,6 +178,9 @@ public class practica1A {
             System.out.println("Elements de la llista: " + Arrays.toString(array));
             System.out.println("Element més gran:" + Arrays.stream(array).max());
             System.out.println("Element més petit:" + Arrays.stream(array).min());
+            //Neteja de la llista
+            array = null;
+            System.out.println("Array netejada correctament");
         }
     }
 
@@ -174,6 +196,7 @@ public class practica1A {
 
     //------------------------------------------------------------------------------------------------------------------
     static void illegalArgumentExceptionsMath(){
+        System.out.println("Inici del mètode illegalArgumentExceptionsMath...");
         Scanner scan = new Scanner(System.in);
         try {
             System.out.println();
@@ -194,8 +217,11 @@ public class practica1A {
             if (causa != null) {
                 System.out.println("       La causa de l'excepció és: " + causa.getMessage());
             } else {
-                System.out.println("       L'excepció no te causa coneguda.");
+                System.out.println("       La causa de l'excepció és: Intent d'obtenir l'arrel quadrada d'un nombre negatiu.");
             }
+        }
+        finally {
+            System.out.println("El mètode illegalArgumentExceptionsMath ha finalitzat.");
         }
     }
 
@@ -209,6 +235,7 @@ public class practica1A {
 
     //------------------------------------------------------------------------------------------------------------------
     static void arithmeticException(){
+        System.out.println("Inici del mètode arithmeticException...");
         int resultat;
         try {
             Scanner scan = new Scanner(System.in);
@@ -227,11 +254,10 @@ public class practica1A {
             if (causa != null) {
                 System.out.println("       La causa de l'excepció és: " + causa.getMessage());
             } else {
-                System.out.println("       L'excepció no te causa coneguda.");
+                System.out.println("       La causa de l'excepció és: Divisor amb valor 0.");
             }
         }finally {
-            // Bloque finally para registrar la finalización del método y restaurar el estado si es necesario
-            System.out.println("El método arithmeticException ha finalizado.");
+            System.out.println("El mètode arithmeticException ha finalitzat.");
             // Realitzem una operació de neteja.
             resultat = 0;
         }
@@ -248,31 +274,100 @@ public class practica1A {
 
     //------------------------------------------------------------------------------------------------------------------
     static void indexOutOfBoundException(){
+        System.out.println("Inici del mètode indexOutOfBoundException...");
         int[] array = {1, 2, 3, 4, 5};
         try {
             System.out.println("accedim a la posicio 6 de l'array!!!");
             int element = array[6]; // Intentem accedir a un índex fora dels límits de l'array
         } catch (IndexOutOfBoundsException e) {
+            System.out.println("---------------------------------------------------------------------------------------");
             System.out.println("S'ha produït un error d'índex fora de límits (IndexOutOfBoundsException): " + e.getMessage());
+            System.out.println("Detalls addicionals:");
+            System.out.println("       Classe de l´excepció: " + e.getClass());
+            Throwable causa = e.getCause();
+            if (causa != null) {
+                System.out.println("       La causa de l'excepció és: " + causa.getMessage());
+            } else {
+                System.out.println("       La causa de l'excepció és: Intent d'accedir a un índex fora dels límits de l'array.");
+            }
+        }finally {
+            // Neteja de l'array per lliurar mèmoria.
+            array = null;
+            System.out.println("L'array s'ha netejat correctament.");
         }
+
     }
+    //------------------------------------------------------------------------------------------------------------------
+
+    //------------------------------------------------------------------------------------------------------------------
     static void nullPointerException(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Inici del mètode nullPointerException...");
         String text = null;
         try {
-            int length = text.length(); // Intentem obtenir la longitud d'una cadena nul·la
+            int length = text.length(); // Intentem obtenir la longitud d'una string
             System.out.println("Longitud del text: " + length);
         } catch (NullPointerException e) {
+            System.out.println("---------------------------------------------------------------------------------------");
             System.out.println("S'ha produït un error de punter nul (NullPointerException): " + e.getMessage());
+            System.out.println("Detalls addicionals:");
+            System.out.println("       Classe de l´excepció: " + e.getClass());
+            Throwable causa = e.getCause();
+            if (causa != null) {
+                System.out.println("       La causa de l'excepció és: " + causa.getMessage());
+            } else {
+                System.out.println("       La causa de l'excepció és: Intent d'obtenir la longitud d'una cadena nul·la.");
+            }
+            System.out.println();
+            System.out.println("Corregirem l'error. Diguem una paraula:  ");
+            text = scan.nextLine();
+            int length = text.length();
+            System.out.println("---------------------------------------------------------------------------------------");
+            System.out.println("Longitud del text: " + length);
+        }finally {
+            System.out.println("Ha finalitzat el mètode nullPointerException.");
+            //Tanquem l'scanner
+            scan.close();
         }
     }
+    //------------------------------------------------------------------------------------------------------------------
 
+    //------------------------------------------------------------------------------------------------------------------
     static void socketException(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Inici del mètode socketException...");
+        System.out.println("Si vols fer saltar l'exception SocketException, desconnectat de la xarxa");
+        System.out.println("Si vols fer saltar l'exception IOException, introdueix un host invàlid");
+        System.out.println("Si vols que la connexió sigui correcte, introdueix un host vàlid");
+        System.out.println("Host: ");
+        String host = scan.nextLine();
         try {
-            connexioSrv("sapalomera.cat", 80); // Intentem connectar-nos a un servidor que pot no existir
+            connexioSrv(host, 80); // Intentem connectar-nos a un servidor que pot no existir
         } catch (SocketException e1) {
+            System.out.println("---------------------------------------------------------------------------------------");
             System.out.println("S'ha produït un error de connexió de xarxa (SocketException): " + e1.getMessage());
+            System.out.println("Detalls addicionals:");
+            System.out.println("       Classe de l´excepció: " + e1.getClass());
+            Throwable causa = e1.getCause();
+            if (causa != null) {
+                System.out.println("       La causa de l'excepció és: " + causa.getMessage());
+            } else {
+                System.out.println("       La causa de l'excepció és: Error de connexió de xarxa.");
+            }
         } catch (IOException e2) {
-            System.out.println("S'ha produït un error d'entrada/sortida (IOException): " + e2.getMessage());
+            System.out.println("---------------------------------------------------------------------------------------");
+            System.out.println("S'ha produït un error d'entrada/sortida (IOException): ");
+            System.out.println("Detalls addicionals:");
+            System.out.println("       Classe de l´excepció: " + e2.getClass());
+            Throwable causa = e2.getCause();
+            if (causa != null) {
+                System.out.println("       La causa de l'excepció és: " + causa.getMessage());
+            } else {
+                System.out.println("       La causa de l'excepció és: Host "+ e2.getMessage() + " invàlid.");
+            }
+
+        }finally {
+            System.out.println("Ha finalitzat el mètode socketException.");
         }
         //podriem fer només un catch amb una sola variable e >>> catch (SocketException | IOException e)
     }
@@ -280,13 +375,29 @@ public class practica1A {
     public static void connexioSrv(String host, int port) throws IOException {
         Socket socket = new Socket(host, port);
         // Aquí es realitzaria la lògica de comunicació amb el servidor
+        System.out.println("Connexió correcte amb el servidor " +host);
         socket.close();
     }
+    //------------------------------------------------------------------------------------------------------------------
+
+    //------------------------------------------------------------------------------------------------------------------
     static void fileNotFoundException(){
         try {
             llegirSolucioExamen("solucio_examen_uf5.txt"); // Intentem llegir un fitxer que no existeix
         } catch (FileNotFoundException e) {
+            System.out.println("---------------------------------------------------------------------------------------");
             System.out.println("No s'ha trobat el fitxer: " + e.getMessage());
+            System.out.println("Detalls addicionals:");
+            System.out.println("       Classe de l´excepció: " + e.getClass());
+            Throwable causa = e.getCause();
+            if (causa != null) {
+                System.out.println("       La causa de l'excepció és: " + causa.getMessage());
+            } else {
+                System.out.println("       La causa de l'excepció és: No es pot trobar el fitxer.");
+            }
+        }
+        finally {
+            System.out.println("Fi del mètode fileNotFoundException");
         }
     }
     public static void llegirSolucioExamen(String nomFitxer) throws FileNotFoundException {
@@ -295,7 +406,9 @@ public class practica1A {
         // Aquí es realitzaria la lectura del fitxer
         scanner.close();
     }
+    //------------------------------------------------------------------------------------------------------------------
 
+    //------------------------------------------------------------------------------------------------------------------
     static void eOFException(){
         String nomFitxer = "notes_finals.dat";
 
@@ -305,9 +418,30 @@ public class practica1A {
                 System.out.println("Valor llegit: " + valor);
             }
         } catch (EOFException e1) {
+            System.out.println("---------------------------------------------------------------------------------------");
             System.out.println("S'ha arribat al final del fitxer: " + e1.getMessage());
+            System.out.println("Detalls addicionals:");
+            System.out.println("       Classe de l´excepció: " + e1.getClass());
+            Throwable causa = e1.getCause();
+            if (causa != null) {
+                System.out.println("       La causa de l'excepció és: " + causa.getMessage());
+            } else {
+                System.out.println("       La causa de l'excepció és: S'ha arribat al final del fitxer.");
+            }
         } catch (IOException e2) {
+            System.out.println("---------------------------------------------------------------------------------------");
             System.out.println("S'ha produït un error d'entrada/sortida: " + e2.getMessage());
+            System.out.println("Detalls addicionals:");
+            System.out.println("       Classe de l´excepció: " + e2.getClass());
+            Throwable causa = e2.getCause();
+            if (causa != null) {
+                System.out.println("       La causa de l'excepció és: " + causa.getMessage());
+            } else {
+                System.out.println("       La causa de l'excepció és: Error d'entrada/sortida.");
+            }
+        }
+        finally {
+            System.out.println("El mètode eOFException ha finalitzat");
         }
     }
 }
